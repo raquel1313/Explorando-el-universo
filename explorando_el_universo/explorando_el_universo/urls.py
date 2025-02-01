154c1e165679b8e82d6ea36de0baa8e132fea2f4
raquel1313 <ra.rehbein@duocuc.cl>
2025-02-01 06:38:03 -0300

    Archivos ev transversal actualizado- Intento subida a vercel

--- explorando_el_universo/explorando_el_universo/urls.py
+++ explorando_el_universo/explorando_el_universo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from plataforma import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,4 @@ urlpatterns = [
     
     
 ]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
